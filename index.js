8a23c935e804e8a37da543868bdd17e41ec267e2
Jeetesh <jeeteshkumar45@gmail.com>
2023-06-10 18:56:28 +0530

    express js complete

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
-const http = require("http");
-const fs = require("fs");
-// const index = fs.readFileSync("index.html", "utf8");
-const index = fs.readFileSync("data.json", "utf8");
-const data = {
-  age: 5,
+const express = require("express");
+const morgan = require("morgan");
+const server = express();
+const port = 8080;
+
+// bodyParser
+server.use(express.json());
+
+// server.use(express.urlencoded())
+server.use(morgan("default"));
+server.use(express.static("public"));
+
+// server.use((req, res, next) => {
+//   console.log(req.method, req.url, req.ip, req.hostname);
+// console.log("LOGGED");
+//   next();
+// });
+
+const auth = (req, res, next) => {
+  // console.log(req.query);
+  // if (req.body.password === "123") {
+  //   next();
+  // } else {
+  //   res.sendStatus(401);
+  // }
+  next();
 };
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log("Server started");
-  //   res.setHeader("Dummy", "Dummy Header");
-  // res.setHeader("Content-Type", "text/html");
-  res.setHeader("Content-Type", "application/json");
-  // res.end(JSON.stringify(data));
-  res.end(index);
+// server.use(auth);
+
+server.get("/products/:id", auth, (req, res) => {
+  console.log(req.params);
+  // res.send("Hello World!");
+  res.json({ type: "GET" });
+  // res.status(200).send("<h1>Hello</h1>");
+  // res.json(products);
+  // res.sendFile("F:Backend\backend_MENindex.html");
+});
+server.post("/post", auth, (req, res) => {
+  res.json({ type: "POST" });
 });
 
-server.listen(8080);
+server.put("/put", (req, res) => {
+  res.json({ type: "PUT" });
+});
+
+server.delete("/delete", (req, res) => {
+  res.json({ type: "DELETE" });
+});
+
+server.patch("/patch", (req, res) => {
+  res.json({ type: "PATCH" });
+});
+
+server.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
