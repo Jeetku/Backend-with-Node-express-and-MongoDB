0ca258296465b017ce12aa10d4ab89ee1c42c01c
Jeetesh <jeeteshkumar45@gmail.com>
2023-06-10 10:26:38 +0530

    First commit Backend

---
+++ index.js
@@ -0,0 +1,14 @@
+const lib = require("./lib.js");
+// import { sum, diff } from "./lib.js";
+
+const fs = require("fs");
+const t1 = performance.now();
+const txt = fs.readFileSync("demo.txt", "utf-8");
+// fs.readFile("demo.txt", "utf-8", (err, text) => {
+//   console.log(text);
+// });
+
+console.log(txt);
+console.log(lib.sum(4, 5), lib.diff(5, 8));
+const t2 = performance.now();
+console.log(t2 - t1);
---
+++ lib.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const diff = (c, d) => {
+  return c - d;
+};
+exports.diff = diff;
+exports.sum = sum;
